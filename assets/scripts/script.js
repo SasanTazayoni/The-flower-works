9cbc21fcbf1a8baf40f1c6c509182bee9d283135
SasanTazayoni <>
2023-06-29 17:13:38 +0000

    Add JS component for testimonials block to dynamically insert text content

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,9 @@
 const collapsibleToggler = document.querySelector('.collapsible__toggler');
 const collapsibleContent = document.querySelector('.collapsible__content');
 const btns = document.querySelectorAll('.button');
+const testimonialsButtons = document.querySelectorAll('[data-testimonials-button]');
+const nextTestimonialButton = document.querySelector('[data-testimonials-button="next"]');
+const testimonialsTabs = document.querySelectorAll('.testimonials__tab');
 
 collapsibleToggler.addEventListener('click', () => {
     collapsibleContent.classList.toggle('collapsible--expanded');
@@ -18,5 +21,19 @@ btns.forEach(btn => {
         setTimeout(() => {
             ripples.remove();
         }, 500);
+    });
+});
+
+testimonialsButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const offset = button.dataset.testimonialsButton === 'next' ? 1 : -1;
+        const testimonials = button.closest('[data-testimonials]').querySelector('[data-testimonials-content]');
+        const activeTestimonial = testimonials.querySelector('[data-active]');
+        let newIndex = [...testimonials.children].indexOf(activeTestimonial) + offset;
+        if (newIndex < 0) newIndex = testimonials.children.length - 1;
+        if (newIndex >= testimonials.children.length) newIndex = 0;
+
+        testimonials.children[newIndex].dataset.active = true;
+        delete activeTestimonial.dataset.active;
     });
 });
