0e866a4546dc8317f9abe90665de8c764eb9ca23
SasanTazayoni <>
2023-07-04 12:33:41 +0000

    Sort JS files into separate folders and link them to the corresponding HTML file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -30,48 +30,6 @@ btns.forEach(btn => {
     });
 });
 
-// Testimonials component
-
-testimonialsButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const offset = button.dataset.testimonialsButton === 'next' ? 1 : -1;
-
-        // Testimonial index
-
-        const testimonials = button.closest('[data-testimonials]').querySelector('[data-testimonials-content]');
-        const activeTestimonial = testimonials.querySelector('[data-active]');
-        let newIndex = [...testimonials.children].indexOf(activeTestimonial) + offset;
-        if (newIndex < 0) newIndex = testimonials.children.length - 1;
-        if (newIndex >= testimonials.children.length) newIndex = 0;
-
-        testimonials.children[newIndex].dataset.active = true;
-        delete activeTestimonial.dataset.active;
-
-        // Tab index
-
-        const tabs = button.closest('[data-testimonials]').querySelector('[data-testimonials-tabs]');
-        const activeTab = tabs.querySelector('[data-active]');
-        let newTabIndex = [...tabs.children].indexOf(activeTab) + offset;
-        if (newTabIndex < 0) newTabIndex = tabs.children.length - 1;
-        if (newTabIndex >= tabs.children.length) newTabIndex = 0;
-
-        tabs.children[newTabIndex].dataset.active = true;
-        delete activeTab.dataset.active;
-
-        button.blur();
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    startTestimonialSlider();
-});
-
-const startTestimonialSlider = () => {
-    setInterval(() => {
-        nextTestimonialButton.click();
-    }, 16000);
-};
-
 // Scroll up link component
 
 window.addEventListener('scroll', () => {
